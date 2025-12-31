2dd90d630cf25053a5bd806de416f48525d606dd
Melbeck777 <molmin729@gmail.com>
2025-12-31 11:38:42 +0900

    [add] Added application and change for connecting postgresql with docker

---
+++ src/main/java/com/booksite/BookSiteApplication.java
@@ -0,0 +1,11 @@
+package com.booksite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookSiteApplication.class, args);
+    }
+}
